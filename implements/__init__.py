86eb7d30fae815399828d4d04a598babce837f76
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-03-24 09:27:43 +0100

    adding new structure

---
+++ implements/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 7 avr. 2014
+@author: Aurelien
+'''
+import sys, os
+
+# Importe tout les sous modules de implements
+from . import analyseaudio
+from . import algotriobjet
+
+# Importe les modules de tests
+from MusiCoreTest.test import *
