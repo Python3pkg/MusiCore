171355c8103b5aecb88322294cc498ff68f3b6cb
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-03-25 11:07:42 +0100

    restructure

--- implements/__init__.py
+++ implements/__init__.py
@@ -6,8 +6,8 @@ Created on 7 avr. 2014
 import sys, os
 
 # Importe tout les sous modules de implements
-from . import analyseaudio
-from . import algotriobjet
+# from .analyseaudio import *
+# from .algotriobjet import *
 
 # Importe les modules de tests
-from MusiCoreTest.test import *
+from tests import *
