1fb6a9f0740960dc71eb6cdda6e635e36699e9da
gerox <aurelien.bettini@telecom-sudparis.eu>
2016-03-27 21:13:28 +0200

    test module anlyseaudio

--- tests/test_analyseaudio.py
+++ tests/test_analyseaudio.py
@@ -3,28 +3,25 @@
 import implements.analyseaudio
 import os
 
-# test
-analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/Deorro.wav", "fichier_csv")
-# print(analyse1.extraire_path())
+# ==========test d'analyse avec 4 musiques===========
+
+analyse1 = implements.analyseaudio.analyse("/home/gerox/Musique/Deorro.wav", "fichier_csv")
 if (analyse1.islineincsc(analyse1.extraire_path()[0]) == False):
     y, sr = analyse1.extrairedatamusic()
     analyse1.analyse_bpm(y, sr)
 
-analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/AC-DC - Highway to Hell.mp3", "fichier_csv")
-# print(analyse1.extraire_path())
+analyse1 = implements.analyseaudio.analyse("/home/gerox/Musique/AC-DC - Highway to Hell.mp3", "fichier_csv")
 if (analyse1.islineincsc(analyse1.extraire_path()[0]) == False):
     y, sr = analyse1.extrairedatamusic()
     analyse1.analyse_bpm(y, sr)
 
-analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/Daft Punk - Get Lucky ft. Pharrell Williams.mp3",
+analyse1 = implements.analyseaudio.analyse("/home/gerox/Musique/Daft Punk - Get Lucky ft. Pharrell Williams.mp3",
                                            "fichier_csv")
-# print(analyse1.extraire_path())
 if (analyse1.islineincsc(analyse1.extraire_path()[0]) == False):
     y, sr = analyse1.extrairedatamusic()
     analyse1.analyse_bpm(y, sr)
 
-analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/Happy - Pharrell Williams.mp3", "fichier_csv")
-# print(analyse1.extraire_path())
+analyse1 = implements.analyseaudio.analyse("/home/gerox/Musique/Happy - Pharrell Williams.mp3", "fichier_csv")
 if (analyse1.islineincsc(analyse1.extraire_path()[0]) == False):
     y, sr = analyse1.extrairedatamusic()
     analyse1.analyse_bpm(y, sr)
