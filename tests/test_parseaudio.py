16dcd627efc70f9dc797d7c4d05f08c56b3b4dd3
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-03-27 21:29:03 +0200

    test_parseaudio

---
+++ tests/test_parseaudio.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+test du module parseaudio
+'''
+
+import implements
+
+############################################################################################
+
+
+# test de parseaudio: parcourt toutes les musiques étant dans le fichier BDDMusic/test_parseaudio
+
+# création de la playlist: playlistbob
+implements.parseaudio.parseaudio("playlistbob")
+
+# création de la playlist: playlist1
+# implements.parseaudio.parseaudio("playlist1")
