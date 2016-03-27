568e33480bbc6fca37d0ebf5094cd3d0624fe730
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-03-27 23:14:44 +0200

    test des modules parseaudio et analyseaudio trahay

--- tests/test_parseaudio.py
+++ tests/test_parseaudio.py
@@ -2,16 +2,33 @@
 
 '''
 test du module parseaudio
+parcourt toutes les musiques étant dans le fichier BDDMusic/test_parseaudio
 '''
 
 import implements
+import csv
 
 ############################################################################################
 
+# test de création de la playlist playlistbob
+# afin de lancer l'analyse, il faut lancer ce programme
+Musique1 = implements.parseaudio.getdirpath() + "/tests/Abba - Waterloo.mp3"
+Musique2 = implements.parseaudio.getdirpath() + "/tests/AC-DC - Highway to Hell.mp3"
+Musique3 = implements.parseaudio.getdirpath() + "/tests/Bruno Mars - Treasure.mp3"
+output_ui = implements.parseaudio.getdirpath() + "/BDDMusic/output_ui"
 
-# test de parseaudio: parcourt toutes les musiques étant dans le fichier BDDMusic/test_parseaudio
+fname = output_ui
+file = open(fname, "w")  # file = open(fname, "rb") python 2.7
+try:
+    writer = csv.writer(file)
+    writer.writerow([Musique1])
+    writer.writerow([Musique2])
+    writer.writerow([Musique3])
 
-# création de la playlist: playlistbob
+finally:
+    file.close()
+
+# test de parse audio
 implements.parseaudio.parseaudio("playlistbob")
 
 # création de la playlist: playlist1
