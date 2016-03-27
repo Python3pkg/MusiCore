298a1d80a030575152d4db838cabcf1ccea31dbe
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-03-27 21:11:28 +0200

    adding: meilleur gestion de l'analyse de toutes les musiques et ajout d'exeptions

--- implements/parseaudio.py
+++ implements/parseaudio.py
@@ -9,18 +9,46 @@ Ce module est initié par l'interface graphique
 
 import implements.analyseaudio
 import csv
+import os
+import sys
 
 
-def parseaudio(pathtocsvfile):
-    # test etant ok
-    analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/Deorro.wav", "fichier_csv")
-    # print(analyse1.extraire_path())
+def getdirpath():
+    '''
+    :return:le chemin du repertoire MusiCore
+    '''
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def parseaudio(nomanalyse):
+    '''
+    exemple de test :
+    analyse1 = implements.analyseaudio.analyse("/home/gerox/Musique/Deorro.wav", "fichier_csv")
     if (analyse1.islineincsc(analyse1.extraire_path()[0]) == False):
         y, sr = analyse1.extrairedatamusic()
         analyse1.analyse_bpm(y, sr)
+    '''
+
+    # on determine les chemins des fichiers necessaire à l'analyse des musiques
+    bddfilepath = getdirpath() + "/BDDMusic/BDDMusic"
+    print("La base de données de musique ce situe: " + bddfilepath)
+
+    output_ui = getdirpath() + "/BDDMusic/output_ui"
+    print("Le fichier à analyser ce situe: " + output_ui)
+
+    pathfichiercsv = getdirpath() + "/BDDMusic/" + nomanalyse
+    print("Le fichier de la playlist est " + pathfichiercsv)
+
+    # verifie si on peut ouvrir le fichier
+    try:
+        with open(output_ui):
+            pass
+    except IOError:
+        print("parseaudio: Erreur! Le fichier output_ui n'a pas pu etre ouvert ou n'existe pas")
+        sys.exit(0)
 
     # ouverture du fichier csv
-    fname = pathtocsvfile
+    fname = output_ui
     file = open(fname, "rt")  # file = open(fname, "rb") python 2.7
     k = 1
 
@@ -33,7 +61,7 @@ def parseaudio(pathtocsvfile):
             numanalyse = str(k)
             analyse = "analyse" + numanalyse
             print(analyse + " : fichier " + row[0])
-            analyse = implements.analyseaudio.analyse(row[0], "fichier_csv")
+            analyse = implements.analyseaudio.analyse(row[0], pathfichiercsv, bddfilepath, )
 
             if (analyse.islineincsc(analyse.extraire_path()[0]) == False):
                 y, sr = analyse.extrairedatamusic()
@@ -45,8 +73,9 @@ def parseaudio(pathtocsvfile):
         file.close()
 
 
-# test
-parseaudio('/home/bettini/PycharmProjects/MusiCore/BDDMusic/test_parseaudio')
+# test de parseaudio: parcourt toutes les musiques étant dans le fichier test_parseaudio
+parseaudio("playlistbob")
+
 
 '''
     analyse1 = implements.analyseaudio.analyse("/home/bettini/Musique/Deorro.wav", "fichier_csv")
