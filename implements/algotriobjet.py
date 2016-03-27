41223ad4ae26b0363935a9775d0523da0df552fb
plichart <victorpli@hotmail.fr>
2016-03-27 22:49:15 +0200

    Update algotriobjet.py

--- implements/algotriobjet.py
+++ implements/algotriobjet.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-# test orienté_objet
-# classe Musique déf par titre, BPM_debut, BPM_fin, pitch
+# coding: utf-8
+#test orienté_objet
+#classe Musique déf par titre, BPM_debut, BPM_fin, pitch
 
-# ------------------------------
-# import des librairies nécessaires
+#------------------------------
+#import des librairies nécessaires
 
 import csv
 
-
-# --------------------
-# Calcule le nombre de ligne du csv
+#--------------------
+#Calcule le nombre de ligne du csv
 def csvcount(filename):
     with open(filename, 'r') as f:
         i = 0
@@ -17,26 +16,16 @@ def csvcount(filename):
             i += 1
     return i
 
-
-# -----------------------------------
-# transfo chaine de caractere '140' en 140 + nombre
+#-----------------------------------
+#transfo chaine de caractere '140' en 140 + nombre
 
 def conversion(chaine):
-    '''
-
-    :param chaine:
-    :return:
-    '''
-    correc = chaine[0:len(chaine)]
-    correcnombre = float(correc)
+    correc=chaine[0:len(chaine)]
+    correcnombre=float(correc)
     return correcnombre
-
-
-# -----------------------
 
 #-----------------------
 #classe musique
-"""expliquer ce que c'est"""
 class Musique:#def classe musique
     #titre, BPM_debut, BPM_fin, pitch
     def _init_(self, titre, emplacement, BPM_moy, BPM_debut, BPM_fin, pitch):#methode constructeur
@@ -48,31 +37,35 @@ class Musique:#def classe musique
        self.pitch = pitch
 
 
-# --------------------------------------------
-# extraction du csv et copie dans classe Musique
-# recuperation de la colonne BPM_debut dans le csv
-
+#--------------------------------------------
+#extraction du csv et copie dans classe Musique
+#recuperation de la colonne BPM_debut dans le csv
+"""tableaucool = []
 fname1 = "testprojetinfo.csv"
 file1 = open(fname1, "rb")
-for i in range(cvscount(testprojetinfo.csv)):
+for i in range (csvcount(testprojetinfo.csv)):
     try:
         reader = csv.reader(file1)
         for row in reader:
             #
             # N'affiche que certaines colonnes
             #
-            print
-            row[0], row[1]
-            titre
-            "i" = Musique(row[0], conversion(row[1]), conversion(row[2]), row[3])
-            i = i + 1
+            print row[0],row[1]
+            temp = Musique(row[0], row[1], conversion(row[2]), conversion(row[3]), conversion(row[4]), row[5])
+            i=i+1
+            tableaucool.append(temp)
 
     finally:
         file1.close()
-    # test daffichage
+    #test daffichage
     titre.titre1
     titree.titre2
-    
+"""
+#----------------------------------------------------
+#test
+
+"""print tableaucool"""
+
 #------------------------------------------------------
 #Tri bpm avec méthode du "plus court chemin"
 #Idée : on calcule la différence de bpm entre chaque musique et on cherche le min de la somme des différences totales
@@ -101,6 +94,7 @@ def BPMmaxi (tableaucool):
     return BPM_max
 
 
+
 def tribpm (tableaucool):
     resultat = []
     resultat.append(BPMmini(tableaucool))
@@ -109,25 +103,22 @@ def tribpm (tableaucool):
     i=0
     compteur = 100000
     print resultat
-    for j in range (len(tableaucool)):
+    for j in range (len(tableaucool)-1):
         while i<(len(tableaucool)):
-            #if tableaucool[i] > sommet:
-            #   print "test1"
             if abs(resultat[j]-tableaucool[i])<=compteur and tableaucool[i]>resultat[j]:
                 compteur = abs(resultat[j]-tableaucool[i])
-                    #if sommet == BPMmaxi(tableaucool):
-                    #    return resultat
                 sommet = tableaucool[i]
-                print "test2"
             i=i+1
         resultat.append(sommet)
         j=j+1
-
+        compteur=100000
+        i=0
     return resultat
 
 
 tableaucool=[130,138,124,112]
 print(tribpm(tableaucool))
+
 
     
     
