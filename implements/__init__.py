e01220f1fd68d72c0cee8e5426d43926693ff334
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-04-20 10:40:07 +0200

    __init__

--- implements/__init__.py
+++ implements/__init__.py
@@ -3,11 +3,7 @@
 Created on 7 avr. 2014
 @author: Aurelien
 '''
-import sys, os
-
-# Importe tout les sous modules de implements
-# from .analyseaudio import *
-# from .algotriobjet import *
+# import sys, os
 
 # Importe les modules de tests
 from tests import *
