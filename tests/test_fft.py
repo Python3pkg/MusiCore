ddfa03f51666258b3a0d167c6e5db85fdd2892c1
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-05-03 14:44:42 +0200

    test analyse de la tonalité

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -4,11 +4,12 @@
 import implements.analyseaudio as analyseaudio
 
 # test numéro 1
+# Mettre le chemin de la musique dont on veut analyser la tonalité dans l'instanciation de l'objet analyse
 
 analyse = analyseaudio.analyse("/home/gerox/Musique/Deorro.wav", "fichier_csv", 'bdd')
 y, s = analyse.extrairedatamusic()
 
 Fs = 44100  # sampling rate
-notefreq = analyse.analysefft(y, Fs, 3,
+notefreq = analyse.analysefft(y, Fs, 50,
                               False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
 analyse.rechercheaccords(notefreq)
