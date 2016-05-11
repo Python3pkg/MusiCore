ac2d6c7885c05959952f59b2b25d0c946ee2287d
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-05-12 00:03:19 +0200

    test fonction de parsing

--- tests/test_parseaudio.py
+++ tests/test_parseaudio.py
@@ -2,7 +2,7 @@
 
 '''
 test du module parseaudio
-parcourt toutes les musiques étant dans le fichier BDDMusic/test_parseaudio
+parcourt toutes les musiques étant dans le fichier database/test_parseaudio
 '''
 
 import implements
@@ -15,7 +15,7 @@ import csv
 Musique1 = implements.parseaudio.getdirpath() + "/tests/Abba - Waterloo.mp3"
 Musique2 = implements.parseaudio.getdirpath() + "/tests/AC-DC - Highway to Hell.mp3"
 Musique3 = implements.parseaudio.getdirpath() + "/tests/Bruno Mars - Treasure.mp3"
-output_ui = implements.parseaudio.getdirpath() + "/BDDMusic/output_ui"
+output_ui = implements.parseaudio.getdirpath() + "/database/output_ui"
 
 fname = output_ui
 file = open(fname, "w")  # file = open(fname, "rb") python 2.7
@@ -28,8 +28,5 @@ try:
 finally:
     file.close()
 
-# test de parse audio
-implements.parseaudio.parseaudio("playlistbob")
-
-# création de la playlist: playlist1
-# implements.parseaudio.parseaudio("playlist1")
+# test de parse audio (le fait de mettre True et True permet de lancer l'analyse bpm et harmonique)
+implements.parseaudio.parseaudio("playlistbob", True, True)
