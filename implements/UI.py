9fe757c12217ab542161a487ba9b7fef9fd91bf8
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-05-13 14:42:40 +0200

    changement option de parsing

--- implements/UI.py
+++ implements/UI.py
@@ -4,6 +4,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import implements.analyseaudio
+import implements.parse_audio_2
 
 ###Fonctions internes au GUI
 
@@ -60,8 +61,12 @@ class Handler:
             analyse = "analyse" + numanalyse
             print(analyse + " : fichier " + i)
             analyse = implements.analyseaudio.analyse(i, nom_analyse.path_to_csv_file, nom_analyse.path_to_database)
-            output_csv = []
 
+            get_bpm = implements.parse_audio_2.parser(nom_analyse, analyse, True, False)
+            print(get_bpm)
+            k += 1
+            '''
+            output_csv = []
             research = nom_analyse.find_title_in_database(analyse.extraire_path()[0])
 
             # Distinction des différentes analyses, bpm ou tonalité suivant les choix de l'utilisateur
@@ -126,10 +131,10 @@ class Handler:
                     nom_analyse.add_list(nom_analyse.path_to_csv_file, bpm + tonalite)
             k += 1
 #        implements.parseaudio.parseaudio(exportPaths(), True, False)
-#        actualize()
+#        actualize()'''
 
     def onHarm(self, button):
-        print('test harmonique')
+        print("vous avez cliqué sur le bouton d'anlyse de la tonalité")
         flag_bpm = False
         flag_tonalite = True
         nomanalyse = 'test'
@@ -144,76 +149,16 @@ class Handler:
             analyse = "analyse" + numanalyse
             print(analyse + " : fichier " + i)
             analyse = implements.analyseaudio.analyse(i, nom_analyse.path_to_csv_file, nom_analyse.path_to_database)
-            output_csv = []
 
-            research = nom_analyse.find_title_in_database(analyse.extraire_path()[0])
-
-            # Distinction des différentes analyses, bpm ou tonalité suivant les choix de l'utilisateur
-
-            if research[0] == True:
-                print(research[3])
-                if research[2] != '//' and research[
-                    3] > 4:  # toutes les données de l'anaylse sont dans la base de données
-                    print('toutes les infomations sur la musique sont présentes')
-                    print(research[1])
-                    output_csv = nom_analyse.get_row_database(research[1])
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-
-                if (research[2] != '//' or research[4] == '**Musique atonale**') and research[
-                    3] < 5:  # il n'y a que le bpm dans la base de données
-                    print("il n'y a que les informations sur l'analyse bpm")
-                    if flag_bpm == True:  # l'utilisateur veut analyser le bpm
-                        output_csv = nom_analyse.get_row_database(research[0])[:4]
-                    if flag_tonalite == True:
-                        y, s = analyse.extrairedatamusic()
-                        Fs = 44100  # sampling rate
-                        notefreq = analyse.analysefft(y, Fs, 50,
-                                                      False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
-                        tonalite = analyse.rechercheaccords(notefreq)
-                        output_csv = output_csv + tonalite
-
-                    nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_database,
-                                         output_csv)  # rajout des données dans la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-
-                if research[2] == '//' or research[
-                    2] == '**Musique atonale**':  # il y seulement l'analyse de la tonalité
-                    print("il n'y a que les informations sur l'analyse de la tonalité")
-                    if flag_bpm == True:
-                        y, sr = analyse.extrairedatamusic()  # extraction des données des musiques
-                        output_csv = analyse.analyse_bpm(y, sr)  # analyse bpm
-                    if flag_tonalite == True:
-                        output_csv = output_csv + nom_analyse.get_row_database(research[0])[1:]
-                    nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_database,
-                                         output_csv)  # rajout des données dans la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-
-            else:
-                print("La musique n'est pas dans la base de données")
-                bpm = []
-                tonalite = []
-                if flag_tonalite != False or flag_bpm != False:
-                    y, sr = analyse.extrairedatamusic()  # extraction des données des musiques
-                    if flag_bpm == True:
-                        bpm = analyse.analyse_bpm(y, sr)  # analyse bpm
-                    if flag_tonalite == True:
-                        Fs = 44100  # sampling rate
-                        notefreq = analyse.analysefft(y, Fs, 50,
-                                                      False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
-                        tonalite = analyse.rechercheaccords(notefreq)
-                    nom_analyse.add_list(nom_analyse.path_to_database, bpm + tonalite)
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file, bpm + tonalite)
+            get_tonalite = implements.parse_audio_2.parser(nom_analyse, analyse, False, True)
+            print(get_tonalite)
             k += 1
 #        implements.parseaudio.parseaudio(exportPaths(), False, True)
 #        actualize()
 
     def onBoth(self, button):
-        print('test des deux')
+
+        print("vous avez cliqué sur le bouton d'anlyse des deux caractéristiques")
         flag_bpm = True
         flag_tonalite = True
         nomanalyse = 'test'
@@ -228,74 +173,10 @@ class Handler:
             analyse = "analyse" + numanalyse
             print(analyse + " : fichier " + i)
             analyse = implements.analyseaudio.analyse(i, nom_analyse.path_to_csv_file, nom_analyse.path_to_database)
-            output_csv = []
 
-            research = nom_analyse.find_title_in_database(analyse.extraire_path()[0])
-
-            # Distinction des différentes analyses, bpm ou tonalité suivant les choix de l'utilisateur
-
-            if research[0] == True:
-                print(research[3])
-                if research[2] != '//' and research[
-                    3] > 4:  # toutes les données de l'anaylse sont dans la base de données
-                    print('toutes les infomations sur la musique sont présentes')
-                    print(research[1])
-                    output_csv = nom_analyse.get_row_database(research[1])
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-                    playlist[k - 1,] = 0
-
-                if (research[2] != '//' or research[4] == '**Musique atonale**') and research[
-                    3] < 5:  # il n'y a que le bpm dans la base de données
-                    print("il n'y a que les informations sur l'analyse bpm")
-                    if flag_bpm == True:  # l'utilisateur veut analyser le bpm
-                        output_csv = nom_analyse.get_row_database(research[0])[:4]
-                    if flag_tonalite == True:
-                        y, s = analyse.extrairedatamusic()
-                        Fs = 44100  # sampling rate
-                        notefreq = analyse.analysefft(y, Fs, 50,
-                                                      False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
-                        tonalite = analyse.rechercheaccords(notefreq)
-                        output_csv = output_csv + tonalite
-
-                    nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_database,
-                                         output_csv)  # rajout des données dans la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-
-                if research[2] == '//' or research[
-                    2] == '**Musique atonale**':  # il y seulement l'analyse de la tonalité
-                    print("il n'y a que les informations sur l'analyse de la tonalité")
-                    if flag_bpm == True:
-                        y, sr = analyse.extrairedatamusic()  # extraction des données des musiques
-                        output_csv = analyse.analyse_bpm(y, sr)  # analyse bpm
-                    if flag_tonalite == True:
-                        output_csv = output_csv + nom_analyse.get_row_database(research[0])[1:]
-                    nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_database,
-                                         output_csv)  # rajout des données dans la base de données
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file,
-                                         output_csv)  # rajout des données dans le csv de l'analyse
-
-            else:
-                print("La musique n'est pas dans la base de données")
-                bpm = []
-                tonalite = []
-                if flag_tonalite != False or flag_bpm != False:
-                    y, sr = analyse.extrairedatamusic()  # extraction des données des musiques
-                    if flag_bpm == True:
-                        bpm = analyse.analyse_bpm(y, sr)  # analyse bpm
-                    if flag_tonalite == True:
-                        Fs = 44100  # sampling rate
-                        notefreq = analyse.analysefft(y, Fs, 50,
-                                                      False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
-                        tonalite = analyse.rechercheaccords(notefreq)
-                    nom_analyse.add_list(nom_analyse.path_to_database, bpm + tonalite)
-                    nom_analyse.add_list(nom_analyse.path_to_csv_file, bpm + tonalite)
+            get_bpm = implements.parse_audio_2.parser(nom_analyse, analyse, True, True)
+            print(get_bpm)
             k += 1
-#        implements.parseaudio.parseaudio(exportPaths(), True, True)
-#        actualize()
 
     def onLaunch(self, button):
         print('test')
