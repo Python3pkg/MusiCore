c1d809464c7fab8ed9929649af766ff3e0e23a25
gerosix <aurelien.bettini@telecom-sudparis.eu>
2016-05-18 16:02:20 +0200

    version finale (manque tout les commentaires)

--- implements/parse_audio_2.py
+++ implements/parse_audio_2.py
@@ -37,18 +37,23 @@ def parser(nom_analyse, analyse, flag_bpm, flag_tonalite):
 
     if research[0] == True:
         print(research[3])
-        if research[2] != '//' and research[3] > 4:  # toutes les données de l'anaylse sont dans la base de données
+        if research[2] != '//' and research[3] > 5:  # toutes les données de l'anaylse sont dans la base de données
             print('toutes les infomations sur la musique sont présentes')
             print(research[1])
             output_csv = nom_analyse.get_row_database(research[1])
             return output_csv
             # nom_analyse.add_list(nom_analyse.path_to_csv_file,output_csv)  # rajout des données dans le csv de l'analyse
 
-        if (research[2] != '//' or research[4] == '**Musique atonale**') and research[
-            3] < 5:  # il n'y a que le bpm dans la base de données
+        if research[2] != '//' and research[3] == 5:  # il n'y a que le bpm dans la base de données
             print("il n'y a que les informations sur l'analyse bpm")
+            bpm = nom_analyse.get_row_database(research[1])[1:4]
+            tonalite = []
+            output_csv = [analyse.extraire_path()[0]]
+            time = nom_analyse.get_row_database(research[1])[-1]
+
             if flag_bpm == True:  # l'utilisateur veut analyser le bpm
-                output_csv = nom_analyse.get_row_database(research[0])[:4]
+                output_csv = output_csv + bpm
+
             if flag_tonalite == True:
                 y, s = analyse.extrairedatamusic()
                 Fs = 44100  # sampling rate
@@ -57,24 +62,28 @@ def parser(nom_analyse, analyse, flag_bpm, flag_tonalite):
                 tonalite = analyse.rechercheaccords(notefreq)
                 output_csv = output_csv + tonalite
 
+
             nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-            nom_analyse.add_list(nom_analyse.path_to_database,
-                                 output_csv)  # rajout des données dans la base de données
-            return output_csv
+            nom_analyse.add_list(nom_analyse.path_to_database, [analyse.extraire_path()[0]] + bpm + tonalite + [
+                str(time)])  # rajout des données dans la base de données
+
+            return output_csv + [str(time)]
             # nom_analyse.add_list(nom_analyse.path_to_csv_file,output_csv)  # rajout des données dans le csv de l'analyse
 
-        if research[2] == '//' or research[
-            2] == '**Musique atonale**':  # il y seulement l'analyse de la tonalité
+        if research[2] == '//' or research[2] == '**Musique atonale**':  # il y seulement l'analyse de la tonalité
             print("il n'y a que les informations sur l'analyse de la tonalité")
+            bpm = []
+            tonalite = []
             if flag_bpm == True:
                 y, sr = analyse.extrairedatamusic()  # extraction des données des musiques
-                output_csv = analyse.analyse_bpm(y, sr)  # analyse bpm
+                bpm = analyse.analyse_bpm(y, sr)  # analyse bpm
             if flag_tonalite == True:
-                output_csv = output_csv + nom_analyse.get_row_database(research[0])[1:]
+                tonalite = [1]  # on met le temps de la musique dedans
             nom_analyse.delete_row_database(research[1])  # on remove la ligne de la base de données
-            nom_analyse.add_list(nom_analyse.path_to_database,
-                                 output_csv)  # rajout des données dans la base de données
-            return output_csv
+            nom_analyse.add_list(nom_analyse.path_to_database, [
+                analyse.extraire_path()[0]] + bpm + tonalite)  # rajout des données dans la base de données
+
+            return [analyse.extraire_path()[0]] + bpm + tonalite
             # nom_analyse.add_list(nom_analyse.path_to_csv_file,output_csv)  # rajout des données dans le csv de l'analyse
 
     else:
@@ -90,7 +99,9 @@ def parser(nom_analyse, analyse, flag_bpm, flag_tonalite):
                 notefreq = analyse.analysefft(y, Fs, 50,
                                               False)  # notesfreq est la matrice contenant les fréquences significatives des k samples analysés
                 tonalite = analyse.rechercheaccords(notefreq)
-            nom_analyse.add_list(nom_analyse.path_to_database, bpm + tonalite + [str(analyse.time)])
+
+            nom_analyse.add_list(nom_analyse.path_to_database,
+                                 [analyse.extraire_path()[0]] + bpm + tonalite + [str(analyse.time)])
             # nom_analyse.add_list(nom_analyse.path_to_csv_file, bpm + tonalite)
-            return bpm + tonalite + [analyse.time]
-    k += 1
+            return [analyse.extraire_path()[0]] + bpm + tonalite + [analyse.time]
+            # k += 1
